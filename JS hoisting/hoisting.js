6935eb753ac78744fc54224f1574e4aab527db5e
PrasannaSelva <prasanna.ap1298@gmail.com>
2021-07-05 11:55:34 +0530

    first commit

---
+++ JS hoisting/hoisting.js
@@ -0,0 +1,24 @@
+ 
+//a=10;
+//var a;
+//a=10;
+//var a=10;
+ console.log(a);
+//var a;
+//a=10;
+var a=10;
+
+
+
+
+
+
+
+//a=10;
+//let a;
+//a=10;
+//let a=10;
+//console.log(a);
+//let a;
+//a=10;
+//let a=10;
